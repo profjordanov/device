2570a8be27ec479316339875358f1d7bdcc2989a
Jordan Jordanov <d_yugioh@abv.bg>
2022-03-23 18:56:35 +0200

    initial commit

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,85 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('pause', onPause, false);
+document.addEventListener('resume', onResume, false);
+document.addEventListener('online', onOnline, false);
+document.addEventListener('offline', onOffline, false);
+
+window.addEventListener("batterystatus", onBatteryStatus, false);
+
+window.addEventListener("batterylow", onBatteryLow, false);
+
+function onDeviceReady() {
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    applyDeviceData(device);
+    checkConnection();
+}
+
+function onBatteryLow(status) {
+    alert("Battery Level Low " + status.level + "%");
+}
+
+function onBatteryStatus(status) {
+    $('#batteryStatus').text(status.level);
+    if (status.isPlugged) {
+        $('#isPluggedLabel').removeClass('ui-checkbox-off');
+        $('#isPluggedLabel').addClass('ui-checkbox-on');
+    } else {
+        $('#isPluggedLabel').removeClass('ui-checkbox-on');
+        $('#isPluggedLabel').addClass('ui-checkbox-off');
+    }
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+    console.log(navigator.connection);
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    $('#connectionType').text(states[networkState]);
+}
+
+
+
+function applyDeviceData(device) {
+    $('#cordovaVersion').text(device.cordova);
+    $('#manufacturer').text(device.manufacturer);
+    $('#isVirtual').text(device.isVirtual);
+    $('#deviceModel').text(device.model);
+    $('#operatingSystem').text(device.platform);
+    $('#uuid').text(device.uuid);
+    $('#serial').text(device.serial);
+    $('#osVersion').text(device.version);
+}
+
+function onPause() {
+    navigator.notification.alert(
+        'You are paused!', // message
+        alertDismissed, // callback
+        'Game Over', // title
+        'Done' // buttonName
+    );
+
+    function alertDismissed() {
+        alert("OK");
+    }
+};
+
+function onResume() {
+    
+};
+
+function onOnline() {
+    checkConnection();
+};
+
+function onOffline() {
+    checkConnection();
+};
